5a313e7f47d924cb910bfe7e82c033641631942d
Luis Rolando Ventocilla Santibañez <lvsmix@gmail.com>
2017-09-05 02:21:58 -0300

    se agrega exports

---
+++ 07-export-node.js
@@ -0,0 +1,12 @@
+var http = require("http");
+fs = require("fs");
+suma= require("./suma");
+
+var html = fs.readFile("./index-async.html",function(err,html){
+        http.createServer(function(request, response) {
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write(suma.suma(1,2).toString());
+        response.write(html);
+        response.end();
+        }).listen(8888);
+});
---
+++ suma.js
@@ -0,0 +1,7 @@
+function suma(a,b){
+    let c = Number(a);
+    let d = Number(b);
+    return c+d;
+}
+
+module.exports.suma = suma;
