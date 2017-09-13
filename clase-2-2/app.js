c7c9c9999d84685b9671b24d946f4246b736b788
LVSMix <lvsmix@gmail.com>
2017-09-13 20:46:08 -0300

    clase-2

--- clase-2-2/app.js
+++ clase-2-2/app.js
@@ -1,17 +1,53 @@
 var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var path = require('path');
+
 var service = require('./services/services');
 var app = express();
 
-service.save();
-service.list();
-service.remove();
-service.update();
+// Muestra un log de todos los request en la consola        
+app.use(morgan('dev'));
 
 app.use(express.static('public'))
+    //app.use('/foo', express.static(__dirname + '/public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// Permite cambiar el HTML con el método POST                   
+app.use(bodyParser.text({ type: 'text/html' }));
+
 
 app.get('/', function(req, res) {
     res.send('Hello World!');
 });
+
+app.get('/pruebaList', function(req, res) {
+    service.list();
+    res.send("Hola mundo");
+});
+
+app.get('/pruebaGet', function(req, res) {
+    var cliente = service.get();
+    res.json(cliente);
+});
+
+app.get('/pruebaSave', function(req, res) {
+    var cliente = service.save();
+    res.json(cliente);
+});
+
+app.get('/pruebaUpdate', function(req, res) {
+    service.update();
+    res.json({ message: 'hooray! welcome to our api!' });
+});
+
+app.get('/pruebaRemove', function(req, res) {
+    service.remove();
+});
+
+
+
 
 app.listen(3000, function() {
     console.log('Example app listening on port 3000!');
--- clase-2-2/services/services.js
+++ clase-2-2/services/services.js
@@ -1,17 +1,25 @@
 function list() {
+    console.log("Estoy en el list");
+}
 
+function get() {
+    var cliente = {
+        "nombre": "Luis",
+        "apellido": "Ventocilla"
+    }
+    return cliente;
 }
 
 function save() {
-
+    console.log("Estoy en el save");
 }
 
 function remove() {
-
+    console.log("Estoy en el remove");
 }
 
 function update() {
-
+    console.log("Estoy en el update");
 }
 
-module.exports = { save, remove, list, update }
+module.exports = { save, remove, list, update, get }
