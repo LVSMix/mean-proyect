64f232b2c104dbaf1e6300d97399084da8199507
LVSMix <lvsmix@gmail.com>
2017-09-19 08:10:43 -0300

    borrado

--- clase-1/03-ejercicio2-node.js
+++
@@ -1,8 +0,0 @@
-var golesEquipo1 = 14;
-var golesEquipo2 = 10;
-
-var ganoEquipo1 = golesEquipo1 > golesEquipo2;
-var empate = golesEquipo1 === golesEquipo2;
-
-console.log(ganoEquipo1); // true
-console.log(empate); // false
